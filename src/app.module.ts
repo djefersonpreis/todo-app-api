c683130e6f40bb8004b0f6302020d209878e62dd
Djeferson Preis <djefersonpreis@gmail.com>
2022-06-18 04:31:01 -0300

    Log Configuration
    Using Winston with Nest framework to make easy logs to the API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { TodosModule } from './todos/todos.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './configs/winston.config';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
 
 @Module({
     imports: [
+        WinstonModule.forRoot(winstonConfig),
         TypeOrmModule.forRoot(typeOrmConfig), 
         TodosModule],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: LoggerInterceptor,
+        },
+    ],
 })
 export class AppModule { }
